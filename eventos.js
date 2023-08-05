6f286b51a4322739f4139579a4cf852d87d03fa1
lucasmiraballes <lucas20miraballes@gmail.com>
2023-08-05 12:52:09 -0300

    click event terminado

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(e){
+
+    const div_boton = document.getElementById("btn_div");
+
+    div_boton.addEventListener("click", () =>{
+
+        alert("Hola! Soy el div");
+    } )
+} )
