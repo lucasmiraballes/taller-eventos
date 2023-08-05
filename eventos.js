df76766079c33465d7d2baccbaeba0e68b815653
lucasmiraballes <lucas20miraballes@gmail.com>
2023-08-05 16:38:20 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,8 +2,16 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     const div_boton = document.getElementById("btn_div");
 
+    const btn_saludar = document.getElementById("btn_saludar");
+
     div_boton.addEventListener("click", () =>{
 
         alert("Hola! Soy el div");
     } )
+
+    btn_saludar.addEventListener("click", function(e) {
+
+        alert("Hola");
+        e.stopPropagation()
+    })
 } )
